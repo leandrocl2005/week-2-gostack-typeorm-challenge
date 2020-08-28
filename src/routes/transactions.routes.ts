f8d8b3498ea76acc6cdde842bb3c5898ea0b656f
leandrocl2005 <leandrocl2005@yahoo.com.br>
2020-08-28 14:25:21 -0300

    5 test passed local

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
@@ -13,10 +15,27 @@ transactionsRouter.get('/', async (request, response) => {
 
 transactionsRouter.post('/', async (request, response) => {
   // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransactionService = new CreateTransactionService();
+
+  const transaction = await createTransactionService.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
+  const deleteTransactionService = new DeleteTransactionService();
+
+  await deleteTransactionService.execute(request.params.id);
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+import { getRepository } from 'typeorm';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute(id: string): Promise<void> {
     // TODO
+    const transactionRepository = getRepository(Transaction);
+    const foundTransaction = await transactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!foundTransaction) throw new AppError('Invalid id!', 400);
+
+    await transactionRepository.remove(foundTransaction);
   }
 }
 
